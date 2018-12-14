0ac7ab3f73dddb6aa0f7958d684116a4d5c2a394
Alexander Zorin <alexanderzorin@Peters-iMac.local>
2018-12-14 13:18:39 +0100

    initial commit renamed packages adjusted input data arguments
    Signed-off-by: Alexander Zorin <azorin@assystem.com>

---
+++ testing/src/main/java/org/eclipse/kuksa/testing/config/GlobalConfiguration.java
@@ -0,0 +1,29 @@
+package org.eclipse.kuksa.testing.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GlobalConfiguration {
+
+	@Value("${DEVICE_ID}")
+	private String deviceId;
+
+	@Value("${SECURITY_TOKEN}")
+	private String securityToken;
+
+	/**
+	 * @return the deviceId
+	 */
+	public String getDeviceId() {
+		return deviceId;
+	}
+
+	/**
+	 * @return the securityToken
+	 */
+	public String getSecurityToken() {
+		return securityToken;
+	}
+
+}
